13c5e400062c12069db8f7f684c69acd992b972e
irfansatya <irfansatya75@gmail.com>
2024-11-21 11:30:27 +0700

    Dua Lagi Selese

--- app/images/publicservice/index.ts
+++ app/images/publicservice/index.ts
@@ -1,9 +1,9 @@
 /** @format */
 
-import asn from "./BPJS.png";
-import bpjs from "./iknow.png";
-import iknow from "./investara.png";
-import investasi from "./mpnasional.png";
-import mpp from "./SmartASN.png";
+import asn from "./SmartASN.png";
+import bpjs from "./BPJS.png";
+import iknow from "./iknow.png";
+import investasi from "./investara.png";
+import mpp from "./mpnasional.png";
 import BtnPublik from "./btnpublik.png";
 export { asn, bpjs, iknow, investasi, mpp, BtnPublik };
